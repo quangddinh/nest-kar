d0653779fa911af224ac2909d699fc9313f24084
quangdinh <quangddinhnt@gmail.com>
2021-11-11 08:04:17 +0700

    finish user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -9,14 +10,29 @@ import { Reports } from './reports/reports.entity';
 @Module({
   // connection sqlite
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Reports],
-      synchronize: true,
-      // automatic update entity to sql table
-      // normally write migration file -> to change the structure of database
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Reports],
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Reports],
+    //   synchronize: true,
+    //   // automatic update entity to sql table
+    //   // normally write migration file -> to change the structure of database
+    // }),
     UserModule,
     ReportsModule,
   ],
